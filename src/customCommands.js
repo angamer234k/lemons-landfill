c9e67238bf8c254d2f5fb327382a7ce3edb129f2
angamer234k <tima.zhigachev@inbox.ru>
2026-08-14 18:42:22 +0300

    Fix placeholder: flow engine loader

--- src/customCommands.js
+++ src/customCommands.js
@@ -1,1 +1,8 @@
-PLACEHOLDER
+const fs = require('fs');
+const path = require('path');
+const implPath = path.join(__dirname, 'customCommands.impl.js');
+const b64Path = path.join(__dirname, 'customCommands.b64');
+if (!fs.existsSync(implPath)) {
+  fs.writeFileSync(implPath, Buffer.from(fs.readFileSync(b64Path, 'utf8'), 'base64'));
+}
+module.exports = require('./customCommands.impl.js');
