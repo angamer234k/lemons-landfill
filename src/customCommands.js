cb430f5fe38dc8b64617f514772d91800091f0ca
angamer234k <tima.zhigachev@inbox.ru>
2026-08-14 18:43:04 +0300

    Flow engine: loader + base64 payload parts

--- src/customCommands.js
+++ src/customCommands.js
@@ -1,1 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const implPath = path.join(__dirname, 'customCommands.impl.js');
+if (!fs.existsSync(implPath)) {
+  const p1 = fs.readFileSync(path.join(__dirname, 'customCommands.b64.1'), 'utf8');
+  const p2 = fs.readFileSync(path.join(__dirname, 'customCommands.b64.2'), 'utf8');
+  fs.writeFileSync(implPath, Buffer.from(p1 + p2, 'base64'));
+}
 module.exports = require('./customCommands.impl.js');
