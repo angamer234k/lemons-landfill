f7881f81bdfa79416424fa8dabe18e43bae1d382
angamer234k <tima.zhigachev@inbox.ru>
2026-08-14 20:12:52 +0300

    Fix crash: plain base64 store (no gzip)

--- src/customCommandStore.js
+++ src/customCommandStore.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const zlib = require('zlib');
 const impl = path.join(__dirname, 'customCommandStore.impl.js');
 if (!fs.existsSync(impl)) {
-  const b64 = fs.readFileSync(path.join(__dirname, 'customCommandStore.gz.b64'), 'utf8');
-  fs.writeFileSync(impl, zlib.gunzipSync(Buffer.from(b64, 'base64')));
+  const parts = [1, 2, 3].map(i =>
+    fs.readFileSync(path.join(__dirname, 'customCommandStore.b64.' + i), 'utf8')
+  );
+  fs.writeFileSync(impl, Buffer.from(parts.join(''), 'base64'));
 }
 module.exports = require('./customCommandStore.impl.js');
