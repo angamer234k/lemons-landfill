bb5e4e7479ffcbf5548a8f3b1d3b3a7b8287bf63
angamer234k <tima.zhigachev@inbox.ru>
2026-08-14 20:15:43 +0300

    Fix Z_DATA_ERROR: plain base64 customCommands (no gzip)

--- src/customCommandStore.js
+++ src/customCommandStore.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const impl = path.join(__dirname, 'customCommandStore.impl.js');
-if (!fs.existsSync(impl)) {
-  const parts = [1, 2, 3].map(i =>
-    fs.readFileSync(path.join(__dirname, 'customCommandStore.b64.' + i), 'utf8')
-  );
-  fs.writeFileSync(impl, Buffer.from(parts.join(''), 'base64'));
-}
+const parts = [1, 2, 3].map(i =>
+  fs.readFileSync(path.join(__dirname, 'customCommandStore.b64.' + i), 'utf8')
+);
+fs.writeFileSync(impl, Buffer.from(parts.join(''), 'base64'));
 module.exports = require('./customCommandStore.impl.js');
--- src/customCommands.js
+++ src/customCommands.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const zlib = require('zlib');
 const impl = path.join(__dirname, 'customCommands.impl.js');
-if (!fs.existsSync(impl)) {
-  const b64 = fs.readFileSync(path.join(__dirname, 'customCommands.gz.b64'), 'utf8');
-  fs.writeFileSync(impl, zlib.gunzipSync(Buffer.from(b64, 'base64')));
-}
+const parts = [1, 2, 3].map(i =>
+  fs.readFileSync(path.join(__dirname, 'customCommands.b64.' + i), 'utf8')
+);
+fs.writeFileSync(impl, Buffer.from(parts.join(''), 'base64'));
 module.exports = require('./customCommands.impl.js');
