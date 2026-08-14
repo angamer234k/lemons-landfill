32bbd52a4d25876cd35ea46818787b613fb958da
angamer234k <tima.zhigachev@inbox.ru>
2026-08-14 18:54:51 +0300

    Complete flow designer UI (3-part gzip payload + loader)

--- src/customCommandHtml.js
+++ src/customCommandHtml.js
@@ -3,7 +3,9 @@ const path = require('path');
 const zlib = require('zlib');
 const impl = path.join(__dirname, 'customCommandHtml.impl.js');
 if (!fs.existsSync(impl)) {
-  const b64 = fs.readFileSync(path.join(__dirname, 'customCommandHtml.gz.b64'), 'utf8');
-  fs.writeFileSync(impl, zlib.gunzipSync(Buffer.from(b64, 'base64')));
+  const parts = [1, 2, 3].map(i =>
+    fs.readFileSync(path.join(__dirname, 'customCommandHtml.gz.b64.' + i), 'utf8')
+  );
+  fs.writeFileSync(impl, zlib.gunzipSync(Buffer.from(parts.join(''), 'base64')));
 }
 module.exports = require('./customCommandHtml.impl.js');
