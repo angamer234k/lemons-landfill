9ff596c6f33fa61b8d769957b9a62df8ef021985
angamer234k <tima.zhigachev@inbox.ru>
2026-08-14 18:52:24 +0300

    Flow command designer UI (gzip payload + loader)

--- src/customCommandHtml.js
+++ src/customCommandHtml.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const zlib = require('zlib');
 const impl = path.join(__dirname, 'customCommandHtml.impl.js');
 if (!fs.existsSync(impl)) {
-  const a = fs.readFileSync(path.join(__dirname, 'customCommandHtml.b64.1'), 'utf8');
-  const b = fs.readFileSync(path.join(__dirname, 'customCommandHtml.b64.2'), 'utf8');
-  fs.writeFileSync(impl, Buffer.from(a + b, 'base64'));
+  const b64 = fs.readFileSync(path.join(__dirname, 'customCommandHtml.gz.b64'), 'utf8');
+  fs.writeFileSync(impl, zlib.gunzipSync(Buffer.from(b64, 'base64')));
 }
 module.exports = require('./customCommandHtml.impl.js');
